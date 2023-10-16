b42646249c005b1f565679f343b7fa60e02cd708
vanya_mem <vanya.cozheurov@yandex.ru>
2023-10-16 19:48:01 +0300

    update AnimalsCage update pox.xml create some beans in AppConfig create Dog class

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,22 @@
 package app.config;
 
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
+    @Bean(name = "Dog")
+    public Dog getDogBean() {
+        return new Dog();
+    }
+
+    @Bean(name = "Timer")
+    public Timer getTimerBean() {
+        return new Timer();
+    }
 }
